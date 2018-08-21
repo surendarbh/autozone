5f6dd5befc2872c7fe2ac16fa38bd6ce1303debf
Antony Nirmal Raj Alphonse <antonynirmalrajalphonse@antonys-mbp.home>
2018-08-20 21:23:40 -0400

    fixed the issue

--- containers/App.js
+++ containers/App.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
-import { Pencilbanner } from '../component/Pencilbanner.js'
+import  Pencilbanner from '../component/Pencilbanner.js'
 import "../css/styles.css"
 
 export default class App extends Component {
 
 
     render() {
-       let cta = true;
-
-        let style = {
-            //fontSize : "10px",
-            //color: "green",
-            //backgroundColor:'#dc3545'
-            fontWeight:'bold'
-        };
+      
 
         return (
 
-            // Banner class can be - a) pencilBannerLight and b)pencilBannerDark
+          
           <Pencilbanner/>
 
 
